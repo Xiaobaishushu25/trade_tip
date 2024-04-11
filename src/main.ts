7e962b44ba4129f3a85497adadc73d2eab71d2d6
Pang <2055761346@qq.com>
2024-04-11 14:49:32 +0800

    K线图

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,49 @@
 import { createApp } from "vue";
 import "./styles.css";
 import App from "./App.vue";
+import * as echarts from 'echarts/core';
+import {
+    ToolboxComponent,
+    ToolboxComponentOption,
+    TooltipComponent,
+    TooltipComponentOption,
+    GridComponent,
+    GridComponentOption,
+    VisualMapComponent,
+    VisualMapComponentOption,
+    LegendComponent,
+    LegendComponentOption,
+    BrushComponent,
+    BrushComponentOption,
+    DataZoomComponent,
+    DataZoomComponentOption
+} from 'echarts/components';
+import {
+    CandlestickChart,
+    CandlestickSeriesOption,
+    LineChart,
+    LineSeriesOption,
+    BarChart,
+    BarSeriesOption
+} from 'echarts/charts';
+import { UniversalTransition } from 'echarts/features';
+import { CanvasRenderer } from 'echarts/renderers';
+
+echarts.use([
+    ToolboxComponent,
+    TooltipComponent,
+    GridComponent,
+    VisualMapComponent,
+    LegendComponent,
+    BrushComponent,
+    DataZoomComponent,
+    CandlestickChart,
+    LineChart,
+    BarChart,
+    CanvasRenderer,
+    UniversalTransition
+]);
+
+//
 
 createApp(App).mount("#app");
